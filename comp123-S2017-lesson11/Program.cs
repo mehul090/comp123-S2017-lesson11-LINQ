e1e4bcfdcc0fe80852fd2341bb4893b777a2aa00
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-25 15:56:24 -0400

    update program to implement

--- comp123-S2017-lesson11/Program.cs
+++ comp123-S2017-lesson11/Program.cs
@@ -8,13 +8,15 @@ using System.Threading.Tasks;
  date:july25,2017
  Description:
  version:0.1*/
-namespace c,omp123_S2017_lesson11
+namespace comp123_S2017_lesson11
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Deck deck = new Deck();
+            Console.WriteLine(deck.ToString());
+                                                    
         }
     }
 }
